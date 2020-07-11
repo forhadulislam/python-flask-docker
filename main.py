37eacbd56b06f24628939dcfa19966e5e84bff95
Sadi <ronex.sadi@yahoo.com>
2020-07-11 17:44:12 +0300

    Updated main.py
    added port parameter for the flask server and updated return values

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ api = Api(app)
 
 class Home(Resource):
     def get(self):
-        return {'hello': 'world'}
+        return {'key': 'value'}
 
 api.add_resource(Home, '/')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=8000)
